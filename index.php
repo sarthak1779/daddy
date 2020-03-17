7e3aee5f25d5fa2b072b84b5911a81097bef4f28
sarthak1779 <dsatmail2@gmail.com>
2020-03-17 09:31:35 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,120 @@
+<!--BYSS-->
+<!DOCTYPE html>
+<!--BYSS-->
+<html>
+<!--BYSS-->
+<link rel="stylesheet" type="text/css" href="style.css">
+<!--BYSS-->
+<head>
+<!--BYSS-->
+	<title>BY SS</title>
+ <!--BYSS--> 
+</head>
+<!--BYSS-->
+<h1>AKSS.IO(BYSS)</h1>
+<!--BYSS-->
+<body id="body">
+ <!--BYSS--> 
+	<div>
+ <!--BYSS-->
+    <blockquote>dfnfnndndnjd<p>CHANNEL<br><br><a href="CHANNEL.html">CLICK HERE TO SEE</a></p><br></blockquote>
+
+  <p>CHROME DINASOUR GAME<br><br><a href="chrome.mp4" download="gameplay of dinasour">CLICK HERE TO DOWNLOAD OR VIEW</a></p><br>   
+  <p>SCRATCH PART-1<br><br><a href="part 1 lvl 1.avi">CLICK HERE TO DOWNLOAD</a></p>
+  <!--BYSS-->
+  <br>
+  <p>SCRATCH PART-2<br><br><a href="part 2 lvl 1.avi">CLICK HERE TO DOWNLOAD</a></p>
+  <br>
+  <!--BYSS-->
+  <P>SCARATCH WINDOWS<br><br><a href="https://downloads.scratch.mit.edu/desktop/Scratch%20Desktop%20Setup%203.6.0.exe">CLICK HERE TO GET SCRATCH</a></P><BR><BR>
+  <!--BYSS-->
+  <p>ESSAY  ON CRICKET <BR><br>
+  <a id="ak" href="crick essay.html">CICK HERE TO SEE THE ESSAY</a>
+  <!--BYSS-->
+</p><br>
+  <p>M.S.C. WORK<br><br><a href="smsc.html">CLICK HERE TO SEE THE WORK</a><br><br></p><br>
+  <!--BYSS-->
+  <p>HINDI REVISION WORK<br><br><a href="main hinriv.html">CLICK HERE TO SEE THE WORK</a></p><br>
+  <!--BYSS-->
+  <p>EXAM TIME TABLE<br><br><a href="timetable.png">CLICK HERE TO DOWNLOAD</a></p><br>
+  <!--BYSS-->
+  <p>MATHS WORK<br><br><a href="math main.html">CLICK HERE TO SEE THE WORK</a></p><br>
+  <!--BYSS-->
+  <p>HOW TO UPLOAD FILE ON OUR WEBSITE<br><br><a href="howup.avi">CLICK HERE TO DOWNLOAD</a></p><br>
+  <!--BYSS-->
+  <p>CALCULATOR<br><br><a href="calc.html">CLICK HERE TO SEE</a></p><br>
+  <!--BYSS-->
+   <p>QUIZ<br><br><a href="quiz part 1.html">CLICK HERE TO PLAY THE QUIZ</a></p><br>
+   <!--BYSS-->
+    <p>ENGLISH WORK<br><br><a href="eng main.html">CLICK HERE TO SEE THE WORK</a></p><br>
+    <p>TIMING<br><br><a href="timing.html">CLICK HERE TO DOWNLOAD OR VIEW</a></p><br>
+     <p>BYSS STORE<br><br><a href="store.html">CLICK HERE TO SEE</a></p><br>
+
+          <p>CSS<br><br><a href="css.html">CLICK HERE TO DOWNLOAD</a></p><br>
+
+    <!--BYSS-->
+    </div>
+    <!--BYSS-->
+    <br>
+    <!--BYSS-->
+    <h3>COMMENT ISSUES,FEEDBACK AND GRAMATICAL ISSUES</h3><br>
+    <!--BYSS-->
+    <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSenXekMnKqW03zIyk0hsHAXeqiUkS3PPkRPgQ4CBHhH_EXvkQ/formResponse">
+      <!--BYSS-->
+    	<label>NAME</label><input type="text" name="entry.193294027" required="" maxlength="20" minlength="5" id="namen"><br><br>
+      <!--BYSS-->
+    	<label>COMMENT</label><input type="text" name="entry.21270842" required="" maxlength="100"><br><br>
+      <!--BYSS-->
+    	<button type="submit" name="submit" class="btn btn-primary" onclick="post()">POST</button>
+    	<h4>$ARTH@K $INGH ALL RIGHTS RESERVED</h4>
+      <!--BYSS-->
+    </form>
+    <div>
+      
+    </div>
+
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+alert('WELCOME')
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e2d584fdaaca76c6fcfe5d8/1e030dou7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
+function post(){
+     alert('posting')
+}
+</script>
+<!--End of Tawk.to Script-->
+<br>
+<br>
+<br>
+<br>
+<p class="about">ABOUT US</p>
+<p class="about">Version: 8.0</p>
+<p class="about">Developed By: (BYSS TECHNOLOGIES)</p>
+<p class="about">Website hosted by: GITHUB</p>
+<p class="about">Whats New: New Levels in quiz and fixed some grammar issues.</p>
+<!--BYSS-->
+
+<form>
+  
+</form>
+
+
+<h3>Note: We have cretaed the apk of our website and will be available soon</h3>
+<!--<p class="style">CHANGE APPEARANCE OF OUR WEBSITE</p>
+   <p class="style-option">BACKGROUND COLOR</p><button onclick="changeGreen()">GREEN</button><button onclick="changeRed()">RED</button><button onclick="changeYellow()">YELLOW</button>
+   UNDER DEVELOPMENT-->
+</body>
+<!--BYSS-->
+</html>
+<!--BYSS-->
---
+++ registration.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost','root');
+if($con){
+	echo" connection successful";
+}else{
+	echo " no connection"; 
+}
+
+mysqli_select_db($con, 'sessionpractical');
+
+$name = $_POST['name'];
+$pass = $_POST['pass'];
+
+
+$q = " select * from signin  where name = '$name' && password = '$pass' ";
+
+$result = mysqli_query($con, $q);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+	echo" duplicate data ";
+}else{
+
+	$qy= " insert  into signin(name , password) values ('$name' , '$pass') ";
+	mysqli_query($con, $qy);
+}
+
+
+
+?>
